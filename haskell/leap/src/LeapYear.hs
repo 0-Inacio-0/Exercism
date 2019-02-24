ecad8e00dab4b4877a33ce6ebe7c269c748c776a
Inácio Moraes <inacio___@hotmail.com>
2019-02-24 18:06:10 -0300

    init

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,8 +2,6 @@ module LeapYear (isLeapYear) where
 --on every year that is evenly divisible by 4
 --  except every year that is evenly divisible by 100
 --      unless the year is also evenly divisible by 400
-xor :: b -> b 
-xnor :: b -> b 
 xor x y = (x or y) and (not x or not y)
 xnor x y = not (xor x y)
 modLeap year num =  year `rem` num == 0

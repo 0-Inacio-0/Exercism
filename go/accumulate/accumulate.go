60ed72bca76dbd99a9fed9a4e0088eb5d9436acd
Inácio Moraes <inacio___@hotmail.com>
2019-02-26 15:08:34 -0300

    Go track - accumulate
    Solution for the accumulate problem.

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -1,1 +1,13 @@
 package accumulate
+
+// Converter is a func that modifies a string
+type converter func(string) string
+
+// Accumulate applies a converter function to a slice of strings and returns the converted slice.
+func Accumulate(convIn []string, conv converter) []string {
+	convOut := make([]string, len(convIn))
+	for i, str := range convIn {
+		convOut[i] = conv(str)
+	}
+	return convOut
+}

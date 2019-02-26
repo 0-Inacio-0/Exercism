65dddcf372de02dd8ebc6ef754f89ff909d75cd1
Inácio Moraes <inacio___@hotmail.com>
2019-02-26 15:55:27 -0300

    Go track - rna-transcription
    Solution for the  rna-transcription problem.

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,17 @@
 package strand
 
-func ToRNA(dna string) string {
+// ToRNA transforms a dna into a rna
+func ToRNA(dna string) (rna string) {
+	for _, ncl := range dna {
+		if ncl == 'G' {
+			rna += "C"
+		} else if ncl == 'C' {
+			rna += "G"
+		} else if ncl == 'T' {
+			rna += "A"
+		} else if ncl == 'A' {
+			rna += "U"
+		}
+	}
+	return rna
 }

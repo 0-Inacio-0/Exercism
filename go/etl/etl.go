068a58b58611c501cb66f207541dfd7191970724
Inácio Moraes <inacio___@hotmail.com>
2019-02-26 15:40:38 -0300

    Go track - etl
    Refactor based on others solution.

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -5,10 +5,9 @@ import "strings"
 // Transform inverts the map from a value equal to a slice of string to a string equal a value.
 func Transform(in map[int][]string) (out map[string]int) {
 	out = make(map[string]int)
-	for i, ele := range in {
+	for scr, ele := range in {
 		for _, str := range ele {
-			str = strings.ToLower(str)
-			out[str] = i
+			out[strings.ToLower(str)] = scr
 		}
 	}
 	return out

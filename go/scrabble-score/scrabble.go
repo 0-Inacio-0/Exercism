363a84ff550571e25faf30006dec86b70cc3a70a
Inácio Moraes <inacio___@hotmail.com>
2019-03-11 17:25:09 -0300

    Go track - Scrabble
    Trimed a line

--- go/scrabble-score/scrabble.go
+++ go/scrabble-score/scrabble.go
@@ -7,8 +7,7 @@ var scoreValues = map[rune]int{'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, '
 	'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
 
 // Score returns the calculated scrabble score of the input string(not case sensitive).
-func Score(str string) int {
-	score := 0
+func Score(str string) (score int) {
 	for _, r := range strings.ToLower(str) {
 		val, found := scoreValues[r]
 		if found {

32a85f3c805192ce3ceb4342b2faa92f4a0c6d1c
Inácio Moraes <22439670+0-Inacio-0@users.noreply.github.com>
2019-04-12 19:25:09 -0300

    Go track - isogram
    Refactored based on mentors tips.

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -4,13 +4,14 @@ import "strings"
 
 // IsIsogram checks if the string does not repeat any letter, spaces and hyphens are allowed to appear multiple times.
 func IsIsogram(str string) bool {
-	out := true
 	str = strings.ToLower(str)
 	for _, r := range str {
-		if r != '-' && r != ' ' && strings.Count(str, string(r)) > 1 {
-			out = false
-			break
+		if r == '-' || r == ' ' {
+			continue
+		}
+		if strings.Count(str, string(r)) > 1 {
+			return false
 		}
 	}
-	return out
+	return true
 }
